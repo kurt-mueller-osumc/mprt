56ee1c46e92921f231610c331761d1a789e0f4fe
Kurt Mueller <kurt.mueller@osumc.edu>
2025-08-06 11:33:19 -0400

    Add validation for MedicalId in Patient module

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -17,3 +17,31 @@ type ITodosApi = {
     getTodos: unit -> Async<Todo list>
     addTodo: Todo -> Async<Todo list>
 }
+
+module Domain =
+    module Patient =
+        type MedicalId = internal MedicalId of string
+
+module Input =
+    module Patient =
+        type MedicalId =
+            | MedicalId of string
+
+            member this.Validate() : Result<Domain.Patient.MedicalId, string list> =
+                match this with
+                | MedicalId id ->
+                    let errors = ResizeArray<string>()
+
+                    if String.IsNullOrWhiteSpace id then
+                        errors.Add "Medical ID cannot be empty"
+                    else
+                        if id.Length <> 12 then
+                            errors.Add "Medical ID must be exactly 12 characters long"
+
+                        if not (id |> Seq.forall Char.IsDigit) then
+                            errors.Add "Medical ID must contain only digits"
+
+                    if errors.Count = 0 then
+                        Ok <| Domain.Patient.MedicalId id
+                    else
+                        Error(errors |> List.ofSeq)
